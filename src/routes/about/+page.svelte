c8be27ca45e1426f7c89b2577a1c609b30abb56e
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-10 15:36:28 +1000

    Renamed

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<main class="bg-lime-800 w-screen h-screen text-white font-primaryfont flex flex-col justify-center items-center gap-y-5" >
+    <h1 class="text-8xl" >
+        Gungi
+    </h1>
+    <div class="flex gap-x-16 mt-12 text-4xl" >
+        <a href="/play" >
+            <button class="round_button" >Play</button>
+        </a>
+        <a href="/about" >
+            <button class="round_button" >How to Play</button>
+        </a>
+    </div>
+</main>
+
+<style lang="postcss" >
+    .round_button {
+        @apply text-lime-800 bg-white px-8 py-2 rounded-full hover:text-white hover:bg-lime-950;
+    }
+</style>
