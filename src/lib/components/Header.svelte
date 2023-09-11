678d49d9b0c2c1a546158ba00f9b67bc871a8754
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-11 12:20:04 +1000

    Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<header class="[&>a]:cursor-pointer text-lime-800 px-8 py-4 rounded-bl-3xl text-xl font-bold gap-x-7 flex tablet:fixed z-10 bg-white right-0" >
+    <a href="/play" >Play</a>
+    <a href="/about" target="_blank" >About</a>
+    <a href="/play" >Contact</a>
+</header>
+
+<style class="postcss" >
+    a:hover {
+        @apply text-green-600;   
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
+<Header />
 <slot />
 
 <script lang="ts" >
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 	import '../proxima_nova.css';
 </script>
