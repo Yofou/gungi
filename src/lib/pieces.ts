b3f74eec673a080756ab6064dd474d3c23b6cf3a
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-11 10:53:15 +1000

    Implementing drag

--- src/lib/pieces.ts
+++ src/lib/pieces.ts
@@ -1,28 +1,31 @@
 export const piece_data: Record<
     string,
-    { amount: number; levels: number; display_name?: string; note?: string }
+    { id: number,amount: number; levels: number; display_name?: string; note?: string }
 > = {
-    majorgeneral: { amount: 4, levels: 3, display_name: 'Major General' },
-    lieutenantgeneral: { amount: 4, levels: 3, display_name: 'Lieutenant General' },
-    general: { amount: 6, levels: 3 },
-    archer: { amount: 2, levels: 3 },
-    knight: { amount: 2, levels: 3 },
-    musketeer: { amount: 1, levels: 3 },
+    majorgeneral: {id: 1, amount: 4, levels: 3, display_name: 'Major General' },
+    lieutenantgeneral: {id: 2, amount: 4, levels: 3, display_name: 'Lieutenant General' },
+    general: {id: 3, amount: 6, levels: 3 },
+    archer: {id: 4, amount: 2, levels: 3 },
+    knight: {id: 5, amount: 2, levels: 3 },
+    musketeer: {id: 6, amount: 1, levels: 3 },
     captain: {
+        id: 7,
         amount: 1,
         levels: 1,
         note: 'The captain takes on the movement ability of the piece (friendly or opponent) that is directly below it.'
     },
-    samurai: { amount: 2, levels: 3 },
+    samurai: {id: 8, amount: 2, levels: 3 },
     fortress: {
+        id: 9,
         amount: 1,
         levels: 1,
         note: "Fortresses can't stack on other pieces; they can only be stacked upon."
     },
-    cannon: { amount: 2, levels: 3 },
-    spy: { amount: 2, levels: 3 },
-    pawn: { amount: 9, levels: 3 },
+    cannon: {id: 10, amount: 2, levels: 3 },
+    spy: {id: 11, amount: 2, levels: 3 },
+    pawn: {id: 12, amount: 9, levels: 3 },
     marshal: {
+        id: 14,
         amount: 1,
         levels: 1,
         display_name: 'Marshal (King)',
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -2,7 +2,7 @@
 	class="bg-lime-800 min-w-screen min-h-screen text-white font-primaryfont flex flex-col justify-center items-center gap-y-5"
 >
 	<div class="flex gap-y-5 w-full desktop:px-24 justify-between">
-		<div class="grid grid-cols-9">
+		<div class="grid grid-cols-9" use:dndzone={{ items: [] }}>
 			{#each { length: 81 } as _, i}
 				<div
 					class="bg-[#eecaa0] border-[#bc7e38] border-t-2 border-r-2 border-solid h-24 aspect-square
@@ -15,15 +15,24 @@
 			<div class="flex flex-col justify-between rounded-3xl bg-lime-950 text-white py-5 px-8">
 				<h4>Tower details</h4>
 			</div>
-			{#each player_data as player}
+			{#each player_data as player, i}
 				<div
 					class="flex flex-col justify-between rounded-3xl gap-y-5 bg-lime-950 text-white py-5 px-8"
 				>
 					<div class="flex justify-between">
 						<h4>{player.name}'s stockpile</h4>
 					</div>
-					<div class="grid grid-cols-10 gap-4">
-						{#each Object.entries(player.piece_data) as [piece_name, piece]}
+					<div
+						class="grid grid-cols-10 gap-4"
+						use:dndzone={{
+							items: Object.values(player.piece_data),
+							type: `stockpile_pieces_${i}`,
+							dropFromOthersDisabled: true
+						}}
+						on:consider={(e) => handleDrag(e,i)}
+						on:finalize={(e) => handleDrag(e,i)}
+					>
+						{#each Object.entries(player.piece_data) as [piece_name, piece] (piece.id)}
 							<div class="h-12 aspect-square cursor-pointer relative">
 								<img
 									class="h-12 block"
@@ -46,6 +55,7 @@
 </main>
 
 <script lang="ts">
+	import { dndzone } from 'svelte-dnd-action';
 	import { piece_data } from '$lib/pieces';
 
 	type BoardPiece = {
@@ -80,6 +90,15 @@
 			piece_data: structuredClone(piece_data)
 		}
 	];
+	
+	function handleDrag(e: CustomEvent,player_number: number) {
+		const keys = Object.keys(player_data[player_number].piece_data);
+		for (let i = 0; i < keys.length; i++ ) {
+			const key = keys[i]
+			player_data[player_number].piece_data[key] = e.detail.items[i];
+		}
+		console.log(player_data)
+	}
 </script>
 
 <style lang="postcss">
